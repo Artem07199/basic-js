0d6e5e9344c76c480e7004fcd1c80e857ade1a3f
Artem07199 <artemlitasov@gmail.com>
2021-09-30 16:16:29 +0300

    add

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  const activ = parseFloat(sampleActivity)
+  if (typeof sampleActivity !== 'string' || activ <= 0 || !isFinite(activ) || activ >= MODERN_ACTIVITY) 
+  return false;
+  const per = 0.693 / HALF_LIFE_PERIOD;
+  const k = Math.log(MODERN_ACTIVITY / activ);
+  const result = Math.ceil(k / per);
+  return result;
 }
--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (date === 0 || date === undefined) return 'Unable to determine the time of year!'
+  if (!(date instanceof Date)) throw new Error('Invalid date!')
+ const month = getMonth() + 1;
+  switch(month) {
+    case 12:
+    case 1:
+    case 2:
+      return 'winter';
+    case 3:
+    case 4:
+    case 5:
+      return 'spring';
+    case 6:
+    case 7:
+    case 8:
+      return 'summer';
+    default: return 'autumn';
+  }
 }
